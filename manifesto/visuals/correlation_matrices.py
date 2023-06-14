19b8b48b93a62444ec2f09d5bc75aac7571974ac
stepheweffie <stephie.maths@gmail.com>
2023-06-14 15:08:48 -0400

    networkx_graph refactor

--- manifesto/visuals/correlation_matrices.py
+++ manifesto/visuals/correlation_matrices.py
@@ -1,18 +1,8 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import pandas as pd
 
 # Assume we have a correlation matrix as a pandas DataFrame
-corr_matrix = pd.DataFrame(
-    np.array([
-        [1.0, 0.5, 0.3],
-        [0.5, 1.0, 0.1],
-        [0.3, 0.1, 1.0]
-    ]),
-    columns=['A', 'B', 'C'],
-    index=['A', 'B', 'C']
-)
 
 
 def networkx_graph(corr_matrx):
@@ -29,16 +19,3 @@ def networkx_graph(corr_matrx):
     nx.draw(G, with_labels=True)
     plt.show()
 
-    for column in corr_matrx:
-        for index in corr_matrx:
-            # We only add an edge between different nodes and if the correlation is above a threshold
-            # if index != column and corr_matrx.loc[index, column]:
-            G.add_edge(index, column, weight=corr_matrx.loc[index, column])
-
-    # Draw the resulting graph
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos)
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edges(G, pos)
-
-    plt.show()
